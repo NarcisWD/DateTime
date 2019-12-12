fe7e02256e960b2d4cf57d52bc894c74c871badb
NarcisWD <cip.ciprian20086@gmail.com>
2019-12-12 11:25:40 +0200

    First 4 tasks

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DateTime
+namespace dateTime
 {
     class Program
     {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Introduceti anul nasterii, luna, ziua si sexul M/F. Exemplu: 1994 11 22 M");
+            string[] str = Console.ReadLine().Split();
+            string data = str[0] + " " + str[1] + " " + str[2];
+            DateTime birthdate = DateTime.Parse(data).Date;
+            Nullable<Gender> gen;
+            if (str[3] == "M" || str[3] == "m")
+            {
+                gen = Gender.Male;
+            }
+            else if(str[3] == "F" || str[3] == "f")
+            {
+                gen = Gender.Female;
+            }
+            else
+            {
+                gen = null;
+            }
+
+            if(gen != null)
+            {
+                if(gen == Gender.Male)
+                {
+
+                }
+            }
+            Console.WriteLine(gen);
+            Console.ReadKey();
         }
     }
 }
